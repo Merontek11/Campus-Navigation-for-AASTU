5fd029574c860157002f90e96ca69267fe4a507a
Merontek11 <merontekleweyni11@gmail.com>
2025-05-26 10:05:28 +0300

    deployment

--- campus_nav_backend/urls.py
+++ campus_nav_backend/urls.py
@@ -2,13 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to Campus Navigation API!")
+
 
 urlpatterns = [
+      path('', home),
       path('admin/', admin.site.urls),
       path('api/', include('apps.core.urls')),
       path('api/', include('apps.places.urls')),
       path('api/', include('apps.navigation.urls')),
-      # path('api/', include('apps.notification.urls')),
+      path('api/', include('apps.feedback.urls')),
       
   ]
 
